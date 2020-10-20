1c81bdcdffe4b3137d58d6925b90b4e6abc15656
王相博 <7685139+pythonistaWong@user.noreply.gitee.com>
2020-10-20 17:43:00 +0800

    init

---
+++ src/components/EachCommodity/index.vue
@@ -0,0 +1,47 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-10-20 17:18:43
+ * @LastEditTime: 2020-10-20 17:39:53
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /flex-table/src/components/EachCommodity.vue
+-->
+<style scoped lang="sass">
+.each_commodity
+  .prop_name
+    text-align: left
+
+  .prop_value
+    display: flex
+</style>
+
+<template lang="pug">
+.each_commodity
+  .prop_name
+    span 规格名
+    input(:value="name")
+  .prop_value
+    span 规格值
+    .value_list
+      input.value_input(
+        v-for="item, index in value"
+        :key="index"
+        :value="item"
+      )
+</template>
+
+<script>
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component({
+  props: {
+    name: String,
+    value: Array
+  }
+})
+class EachCommodity extends Vue {
+}
+
+export default EachCommodity
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,70 @@
 <!--
  * @Author: your name
  * @Date: 2020-10-20 17:06:27
- * @LastEditTime: 2020-10-20 17:10:45
+ * @LastEditTime: 2020-10-20 17:38:27
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /flex-table/src/views/Home.vue
 -->
 <style scoped lang="sass">
 .app_container
+  .commodity
+    display: flex
+
+  .detail
+    display: flex
 
 </style>
 
 <template lang="pug">
-.app_container 222
+.app_container
+  .commodity
+    .title 商品规格:
+    .content
+      .commodity_list
+        EachCommodity(
+          v-for="item in commodityList"
+          :key="item.id"
+          :name="item.name"
+          :value="item.value"
+        )
+      .commodity_settings
+  .detail
+    .title 规格明细:
+    .content
+      .detail_list list
 
 </template>
 
 <script>
 import Vue from 'vue'
 import Component from 'vue-class-component'
+import EachCommodity from '../components/EachCommodity'
 
-@Component
+@Component({
+  components: {
+    EachCommodity
+  }
+})
 class Hello extends Vue {
+  commodityList = [
+    {
+      name: 'color',
+      value: [
+        'red',
+        'grey',
+        'red'
+      ]
+    },
+    {
+      name: 'size',
+      value: [
+        'small',
+        'middle',
+        'big'
+      ]
+    }
+  ]
 }
 
 export default Hello
